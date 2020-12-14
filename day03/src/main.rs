0a9ddc69f7928318016fd78b104fe1b7560f09e4
Walter Vaughan <wbv@users.noreply.github.com>
2020-12-13 20:14:22 -0500

    day03 small progress

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -3,7 +3,18 @@ use std::io::{BufReader, BufRead};
 
 fn main() -> io::Result<()> {
 	/* get standard input as a buffered reader */
-	let buf = BufReader::new(io::stdin());
+	let mut buf = BufReader::new(io::stdin());
+
+	/* get the first line */
+	let mut linebuf = String::new();
+	buf.read_line(&mut linebuf).expect("Failed to read first line");
+	let firstline = linebuf.trim();
+	println!("{}", firstline);
+
+	/* get the number of characters in the first line */
+	//println!("{}", firstline.len());
+	let mapwidth = firstline.len();
+	println!("{}", mapwidth);
 
 	Ok(())
 }
