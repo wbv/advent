aa6d6ce7488734ce658b57683daba1898a8fd7ad
Walter Vaughan <wbv@users.noreply.github.com>
2020-12-14 00:16:31 -0500

    ugly day03 completed

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -2,6 +2,27 @@ use std::io;
 use std::io::{Read};
 
 
+/* count the number of trees ('#') along the path `right, down` on a map of
+ * width `width` represented by the iterator across u8 slices of `lines` */
+fn count_trees<'a, I>(lines: I, width: usize, right: usize, down: usize)
+-> usize 
+where
+	I: Iterator<Item = &'a [u8]>,
+{
+	let trees =	lines
+		.step_by(down)
+		.enumerate()
+		.filter(|(i, line)| line.get(i*right % width) == Some(&('#' as u8)))
+		.count();
+	println!(
+		"{:4} trees found along path of down {} and right {}",
+		trees,
+		down,
+		right
+	);
+	return trees;
+}
+
 fn main() -> io::Result<()> {
 	/* just buffer the whole input file at once */
 	let mut buf: Vec<u8> = Vec::new();
@@ -12,23 +33,24 @@ fn main() -> io::Result<()> {
 	let mapwidth = buf.iter().position(|&x| x == '\n' as u8).unwrap();
 	println!("Map is {} bytes wide.", mapwidth);
 
-	/* get a buffer with all newlines removed */
+	/* look at each line as a separate vec of u8's, no newlines */
 	let lines = buf.split(|x| *x == '\n' as u8);
 
 	/* count the number of trees '#' along the path, where the path will be
 	 * down a line, then right by 3, with the map repeating infinitely
 	 * rightwards */
-	let right = 3;
-	let trees = lines
-		.enumerate()
-		.filter(|(i, line)| line.get(i*right % mapwidth) == Some(&('#' as u8)))
-		.count();
 
-	println!(
-		"Found {} trees along path of down 1 and right {}",
-		trees,
-		right
-	);
+	/* formulate paths as (right amount, down amount) pairs */
+	let paths = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)];
+	let lines = buf.split(|x| *x == '\n' as u8);
 
+	/* compute the product of the number of trees found in each path strat */
+	let product = paths
+		.iter()
+		.map(|path| count_trees(buf.split(|x| *x == '\n' as u8), mapwidth, path.0, path.1))
+		.fold(1, |acc, x| acc * x);
+
+	println!("Product of all paths is {}", product);
+	
 	Ok(())
 }
