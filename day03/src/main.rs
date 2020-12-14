a5691a268b97dfbd044fea3b97c2c16cd1b6d9fc
Walter Vaughan <wbv@users.noreply.github.com>
2020-12-14 00:23:38 -0500

    cleaned up day03

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -4,22 +4,18 @@ use std::io::{Read};
 
 /* count the number of trees ('#') along the path `right, down` on a map of
  * width `width` represented by the iterator across u8 slices of `lines` */
-fn count_trees<'a, I>(lines: I, width: usize, right: usize, down: usize)
--> usize 
-where
-	I: Iterator<Item = &'a [u8]>,
-{
+fn count_trees(lines: &Vec<&[u8]>, width: usize, right: usize, down: usize)
+               -> usize {
 	let trees =	lines
+		.iter()
 		.step_by(down)
 		.enumerate()
 		.filter(|(i, line)| line.get(i*right % width) == Some(&('#' as u8)))
 		.count();
-	println!(
-		"{:4} trees found along path of down {} and right {}",
-		trees,
-		down,
-		right
-	);
+
+	println!("{:4} trees found along path of down {} and right {}",
+	         trees, down, right);
+
 	return trees;
 }
 
@@ -34,20 +30,15 @@ fn main() -> io::Result<()> {
 	println!("Map is {} bytes wide.", mapwidth);
 
 	/* look at each line as a separate vec of u8's, no newlines */
-	let lines = buf.split(|x| *x == '\n' as u8);
-
-	/* count the number of trees '#' along the path, where the path will be
-	 * down a line, then right by 3, with the map repeating infinitely
-	 * rightwards */
+	let lines = buf.split(|x| *x == '\n' as u8).collect::<Vec<_>>();
 
 	/* formulate paths as (right amount, down amount) pairs */
-	let paths = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)];
-	let lines = buf.split(|x| *x == '\n' as u8);
+	let paths = [(3, 1), (1, 1), (5, 1), (7, 1), (1, 2)];
 
 	/* compute the product of the number of trees found in each path strat */
 	let product = paths
 		.iter()
-		.map(|path| count_trees(buf.split(|x| *x == '\n' as u8), mapwidth, path.0, path.1))
+		.map(|path| count_trees(&lines, mapwidth, path.0, path.1))
 		.fold(1, |acc, x| acc * x);
 
 	println!("Product of all paths is {}", product);
