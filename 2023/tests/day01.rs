e38960da9208859c42441e7c1f63be064c4eaece
Walter B. Vaughan <wbv@users.noreply.github.com>
2023-12-07 13:47:44 -0500

    2023/day01: minor formating

--- 2023/tests/day01.rs
+++ 2023/tests/day01.rs
@@ -10,27 +10,31 @@ fn get_reader<P: AsRef<Path> + Copy + Display>(path: P) -> BufReader<File> {
 #[test]
 fn example1() {
     let input = get_reader("inputs/day01/example.txt");
-    let answer = solve_part1(input).expect("failed to get a solution");
+    let answer = solve_part1(input)
+        .expect("failed to get a solution");
     assert_eq!(answer, 142);
 }
 
 #[test]
 fn part1() {
     let input = get_reader("inputs/day01/part1.txt");
-    let answer = solve_part1(input).expect("failed to get a solution");
+    let answer = solve_part1(input)
+        .expect("failed to get a solution");
     assert_eq!(answer, 53194);
 }
 
 #[test]
 fn example2() {
     let input = get_reader("inputs/day01/example2.txt");
-    let answer = solve_part2(input).expect("failed to get a solution");
+    let answer = solve_part2(input)
+        .expect("failed to get a solution");
     assert_eq!(answer, 281);
 }
 
 #[test]
 fn part2() {
     let input = get_reader("inputs/day01/part2.txt");
-    let answer = solve_part2(input).expect("failed to get a solution");
+    let answer = solve_part2(input)
+        .expect("failed to get a solution");
     assert_eq!(answer, 54249);
 }
