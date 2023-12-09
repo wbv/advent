08579d26d6d0b2df54fe22003ff69f934cba57b3
Walter B. Vaughan <wbv@users.noreply.github.com>
2023-12-09 14:43:24 -0500

    2023/day04: part1 done

--- 2023/src/lib.rs
+++ 2023/src/lib.rs
@@ -3,6 +3,7 @@
 pub mod day01;
 pub mod day02;
 pub mod day03;
+pub mod day04;
 
 pub use clap::{Parser, Subcommand};
 pub use log::*;
---
+++ 2023/tests/day04.rs
@@ -0,0 +1,52 @@
+use aoc2023::day04::*;
+
+use std::{fmt::Display, path::Path, io::BufReader, fs::File};
+
+fn log_init() {
+    let _ = env_logger::builder().format_timestamp(None).try_init();
+}
+
+fn get_reader<P: AsRef<Path> + Copy + Display>(path: P) -> BufReader<File> {
+    let input = File::open(path).expect(format!("failed to open test input file: {path}").as_str());
+    BufReader::new(input)
+}
+
+#[test]
+fn example1() {
+    log_init();
+
+    let input = get_reader("inputs/day04/example.txt");
+    let answer = solve_part1(input)
+        .expect("failed to get a solution");
+    assert_eq!(answer, 13);
+}
+
+#[test]
+fn part1() {
+    log_init();
+
+    let input = get_reader("inputs/day04/part1.txt");
+    let answer = solve_part1(input)
+        .expect("failed to get a solution");
+    assert_eq!(answer, 20667);
+}
+
+//#[test]
+//fn example2() {
+//    log_init();
+//
+//    let input = get_reader("inputs/day04/example.txt");
+//    let answer = solve_part2(input)
+//        .expect("failed to get a solution");
+//    assert_eq!(answer, 467835);
+//}
+//
+//#[test]
+//fn part2() {
+//    log_init();
+//
+//    let input = get_reader("inputs/day04/part2.txt");
+//    let answer = solve_part2(input)
+//        .expect("failed to get a solution");
+//    assert_eq!(answer, 82824352);
+//}
