647e35ed17d5623de909796f1340b00e108cd6e1
Walter B. Vaughan <wbv@users.noreply.github.com>
2023-12-21 22:52:30 -0500

    2023: update skeleton

--- 2023/src/skeleton-day00.rs
+++ 2023/src/skeleton-day00.rs
@@ -1,15 +1,17 @@
 #![cfg(not(doctest))]
 
 use super::*;
+type AdvInt = usize;
 
-pub fn solve_part1<B: BufRead>(input: B) -> std::io::Result<isize> {
-    let mut lines = input.lines();
-
-    unimplemented!()
+pub fn solve_part1<L: IntoIterator<Item = String>>(input: L) -> AdvInt {
+    todo!()
 }
 
-pub fn solve_part2<B: BufRead>(input: B) -> std::io::Result<isize> {
-    let mut lines = input.lines();
-
-    unimplemented!()
+pub fn solve_part2<L: IntoIterator<Item = String>>(input: L) -> AdvInt {
+    todo!()
 }
+
+testcase!(ex1, solve_part1, "example", 0);
+testcase!(part1, solve_part1, "input", 0);
+testcase!(ex2, solve_part2, "example", 0);
+testcase!(part2, solve_part2, "input", 0);
