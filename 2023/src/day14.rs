f70ab75d840642ccd5663fbeaf6883f8dcf7592b
Walter B. Vaughan <wbv@users.noreply.github.com>
2023-12-27 15:46:27 -0500

    2023/day14: done

--- 2023/src/day14.rs
+++ 2023/src/day14.rs
@@ -19,7 +19,7 @@
 //! the platform, and the shape of the platform controls which ropes move and ultimately the focus
 //! of the dish.
 
-use std::ops::{Index, IndexMut};
+use std::{ops::{Index, IndexMut}, collections::HashMap};
 use super::*;
 type AdvInt = usize;
 
@@ -83,23 +83,105 @@ type AdvInt = usize;
 /// Tilt the platform so that the rounded rocks all roll north. Afterward, what is the total load
 /// on the north support beams?
 pub fn solve_part1<L: IntoIterator<Item = String>>(input: L) -> AdvInt {
-    input.into_iter()
-        .collect::<Vec<String>>()
-        .split(|line| line.is_empty())
-        .map(|lines| {
-            let mut platform: Platform = lines.into();
-            debug!("Platform:\n{platform:?}");
-            platform.tilt(North);
-            debug!("Platform (tilted):\n{platform:?}");
-            platform.weigh(North)
-        })
-        .sum()
+    let input: Vec<String> = input.into_iter().collect();
+    let mut platform: Platform = input.as_slice().into();
+    debug!("Platform:\n{platform:?}");
+    platform.tilt();
+    debug!("Platform (tilted):\n{platform:?}");
+    platform.weigh()
 }
 
+/// # Spin Cycles
+///
+/// The parabolic reflector dish deforms, but not in a way that focuses the beam. To do that,
+/// you'll need to move the rocks to the edges of the platform. Fortunately, a button on the side
+/// of the control panel labeled "spin cycle" attempts to do just that!
+///
+/// Each cycle tilts the platform four times so that the rounded rocks roll north, then west, then
+/// south, then east. After each tilt, the rounded rocks roll as far as they can before the
+/// platform tilts in the next direction. After one cycle, the platform will have finished rolling
+/// the rounded rocks in those four directions in that order.
+///
+/// Here's what happens in the example above after each of the first few cycles:
+///
+/// After 1 cycle:
+/// ```
+/// .....#....
+/// ....#...O#
+/// ...OO##...
+/// .OO#......
+/// .....OOO#.
+/// .O#...O#.#
+/// ....O#....
+/// ......OOOO
+/// #...O###..
+/// #..OO#....
+/// ```
+///
+/// After 2 cycles:
+/// ```
+/// .....#....
+/// ....#...O#
+/// .....##...
+/// ..O#......
+/// .....OOO#.
+/// .O#...O#.#
+/// ....O#...O
+/// .......OOO
+/// #..OO###..
+/// #.OOO#...O
+/// ```
+///
+/// After 3 cycles:
+/// ```
+/// .....#....
+/// ....#...O#
+/// .....##...
+/// ..O#......
+/// .....OOO#.
+/// .O#...O#.#
+/// ....O#...O
+/// .......OOO
+/// #...O###.O
+/// #.OOO#...O
+/// ```
+///
+/// This process should work if you leave it running long enough, but you're still worried about
+/// the north support beams. To make sure they'll survive for a while, you need to calculate the
+/// total load on the north support beams after 1000000000 cycles.
+///
+/// In the above example, after 1000000000 cycles, the total load on the north support beams is 64.
+///
+/// Run the spin cycle for 1000000000 cycles. Afterward, what is the total load on the north
+/// support beams?
 pub fn solve_part2<L: IntoIterator<Item = String>>(input: L) -> AdvInt {
-    todo!()
+    let input: Vec<String> = input.into_iter().collect();
+    let mut platform: Platform = input.as_slice().into();
+    let mut seen_platforms = HashMap::<Vec<u8>, u32>::new();
+    debug!("Platform:\n{platform:?}");
+    seen_platforms.insert(platform.map.clone(), 0);
+
+    const CYCLES: u32 = 1_000_000_000;
+    for iteration in 1..=CYCLES {
+        platform.spin_cycle();
+        debug!("Platform (cycle: {iteration}):\n{platform:?}\nWeight: {}", platform.weigh());
+        // check if we hit a loop and "fast-forward"
+        if let Some(prev_iteration) = seen_platforms.insert(platform.map.clone(), iteration) {
+            let repeating = iteration - prev_iteration;
+            let remaining = (CYCLES - iteration) % repeating;
+            info!("Same platform on cycle {iteration} as on prior cycle {prev_iteration}.");
+            info!("Fast-forwarding {} cycles, then cycling {remaining} more times...", CYCLES - iteration);
+            for _ in 1..=remaining {
+                platform.spin_cycle();
+            }
+            break;
+        }
+    }
+
+    platform.weigh()
 }
 
+#[derive(Clone)]
 struct Platform {
     map: Vec<u8>,
     width: usize,
@@ -110,13 +192,12 @@ impl From<&[String]> for Platform {
     fn from(lines: &[String]) -> Self {
         let mut width = 0;
         let mut height = 0;
-        let map: Vec<u8> = lines.into_iter()
-            .map(|line| {
+        let map: Vec<u8> = lines.iter()
+            .flat_map(|line| {
                 height += 1;
                 width = width.max(line.len());
                 line.as_bytes().to_owned()
             })
-            .flatten()
             .collect();
 
         // verify flattening assumptions
@@ -130,15 +211,8 @@ impl From<&[String]> for Platform {
     }
 }
 
-#[derive(Debug, PartialEq)]
-enum Direction {
-    North
-}
-use Direction::*;
-
 impl Platform {
-    fn tilt(&mut self, dir: Direction) {
-        debug_assert_eq!(dir, North);
+    fn tilt(&mut self) {
         // for each column
         for x in 0..self.width {
             // for each row in that column, try to move each found stone up (until they cant)
@@ -156,19 +230,37 @@ impl Platform {
                         // swap and resume stone search
                         self[(x, newrow)] = b'O';
                         self[(x, y)] = b'.';
-                        debug!("moved stone at {x}, {y} to {x}, {newrow}");
+                        //debug!("moved stone at {x}, {y} to {x}, {newrow}");
                     } else {
                         // note that we can't move the stone at all (found an obstacle)
-                        debug!("couldn't move stone at {x}, {y}");
+                        //debug!("couldn't move stone at {x}, {y}");
                     }
                 }
             }
         }
     }
 
-    fn weigh(&mut self, dir: Direction) -> usize {
-        debug_assert_eq!(dir, North);
+    /// rotates the map clockwise by 90 degrees
+    fn rotate(&mut self) {
+        let old = self.clone();
+        for j in 0..old.height {
+            for i in 0..old.width {
+                self[(old.height - 1 - j, i)] = old[(i, j)];
+            }
+        }
 
+        (self.width, self.height) = (self.height, self.width);
+    }
+
+    /// (tilt then rotate) x4
+    fn spin_cycle(&mut self) {
+        for _ in 1..=4 {
+            self.tilt();
+            self.rotate();
+        }
+    }
+
+    fn weigh(&mut self) -> usize {
         // on each level (top to bottom)
         (0..self.height).map(|row| {
             // count the number of round stones
@@ -178,7 +270,7 @@ impl Platform {
                 .count();
             // compute (and return) the "weighted" count
             let level = self.height - row;
-            debug!("on level: {level} found {found} stones");
+            //debug!("on level: {level} found {found} stones");
             found * level
         }).sum()
     }
@@ -187,15 +279,21 @@ impl Platform {
 impl Index<(usize, usize)> for Platform {
     type Output = u8;
     fn index(&self, index: (usize, usize)) -> &Self::Output {
-        &self.map[index.0 + index.1 * self.width]
+        if index.0 >= self.width || index.1 >= self.height {
+            panic!("invalid index into platform map");
+        } else {
+            &self.map[index.0 + index.1 * self.width]
+        }
     }
 }
 
 impl IndexMut<(usize, usize)> for Platform {
     fn index_mut(&mut self, index: (usize, usize)) -> &mut Self::Output {
-        self.map
-            .get_mut(index.0 + index.1 * self.width)
-            .expect("invalid index into platform map")
+        if index.0 >= self.width || index.1 >= self.height {
+            panic!("invalid index into platform map");
+        } else {
+            &mut self.map[index.0 + index.1 * self.width]
+        }
     }
 }
 
@@ -205,7 +303,7 @@ impl std::fmt::Debug for Platform {
             for i in 0..self.width {
                 write!(f, "{}", self.map[i + self.width * j] as char)?
             }
-            write!(f, "\n")?
+            writeln!(f)?
         }
         Ok(())
     }
@@ -213,5 +311,5 @@ impl std::fmt::Debug for Platform {
 
 testcase!(ex1, solve_part1, "example", 136);
 testcase!(part1, solve_part1, "input", 109665);
-//testcase!(ex2, solve_part2, "example", 0);
-//testcase!(part2, solve_part2, "input", 0);
+testcase!(ex2, solve_part2, "example", 64);
+testcase!(part2, solve_part2, "input", 96061);
